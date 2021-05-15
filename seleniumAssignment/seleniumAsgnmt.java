a7ee05c0dee998b5bad3b97dec5e830afd07a683
PratapV <PratapV@HBADT4950172.bsg.ad.adp.com>
2021-05-15 14:18:10 +0530

    Selenium

--- seleniumAssignment/seleniumAsgnmt.java
+++ seleniumAssignment/seleniumAsgnmt.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class seleniumAsgnmt {
 
+	
 	public static void main(String [] args) throws InterruptedException{
 	                  //System.setProperty("webdriver.chrome.driver",path of executable file "Chromedriver.exe")
 					  @BeforeSuite
